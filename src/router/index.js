730b6aac498f8030990542b08f0dadbbbe361bf7
Shields <corey.shields@duckcreek.com>
2018-09-18 20:05:10 -0400

    first comit

---
+++ src/router/index.js
@@ -0,0 +1,27 @@
+import Vue from 'vue'
+import Router from 'vue-router'
+import Hello from '@/components/Hello'
+import Login from '@/components/Login'
+import Signup from '@/components/Signup'
+
+Vue.use(Router)
+
+export default new Router({
+  routes: [
+    {
+		path: '/hello',
+		name: 'Hello',
+		component: Hello
+    },
+    {
+    	path: '/login',
+    	name: 'Login',
+      	component: Login
+    },
+    {
+    	path: '/signup',
+    	name: 'Signup',
+      	component: Signup
+    }
+  ]
+})
